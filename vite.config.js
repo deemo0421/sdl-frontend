1fe31512ff92c6de5b4735ae987780fa0ba4664e
otterrr <b211119012@gmail.com>
2023-07-08 00:34:12 +0800

    vite server config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    host:"0.0.0.0",
+    port:5173,
+  },
+  watch: {
+    usePolling: true
+  },
   plugins: [
     react(),
     nodePolyfills({
